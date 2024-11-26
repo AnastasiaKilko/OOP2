5e78afd168995ec8525eb91282c6b7e57881f52c
Анастасия Килко <gr431_kiava@lab-402-2.tomtit-tomsk.ru>
2024-11-26 10:39:33 +0700

    admin panel created

--- designpro/designapp/admin.py
+++ designpro/designapp/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Application, Category, Status
+from .models import Application, Category
 
 # Register your models here.
-admin.site.register(Application)
+# admin.site.register(Application)
 admin.site.register(Category)
-admin.site.register(Status)
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'category', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
--- designpro/designapp/models.py
+++ designpro/designapp/models.py
@@ -6,19 +6,8 @@ class Application(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=1000, help_text="Enter a brief description of the application")
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
-    status = models.ForeignKey('Status', on_delete=models.SET_NULL, null=True)
-    time = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.name
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-class Status(models.Model):
     APP_STATUS = (
         ('n', 'New'),
         ('a', 'Accepted'),
@@ -28,4 +17,10 @@ class Status(models.Model):
     status = models.CharField(max_length=1, choices=APP_STATUS, default='n', help_text="The status of the application")
 
     def __str__(self):
-        return self.status
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
